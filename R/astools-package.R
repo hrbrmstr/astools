c59aaf6d595277650130d2cd2e0fc0f19796e96c
hrbrmstr <bob@rud.is>
2018-12-06 11:45:05 -0500

    R package repo initialization complete

---
+++ R/astools-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/astools>
+#' - BugReports: <https://gitlab.com/hrbrmstr/astools/issues>
+#' 
+#' @md
+#' @name astools
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
